79069aa8a7a4fd3368ddfd6182d831b28107d6f1
Zuhrizal <zuhrizalmuhammad.xrpl@gmail.com>
2025-02-06 17:04:51 +0700

    Set bootstrap/app.php to only require not require_once

--- public/index.php
+++ public/index.php
@@ -48,7 +48,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
